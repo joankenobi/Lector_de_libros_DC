9b23da0c1f3b127578210c723a970a67afab6a19
JoanKenobi <joan1233blanco@gmail.com>
2021-11-12 09:47:51 -0400

    aplicando el modulo Counter

--- counter.py
+++ counter.py
@@ -1,16 +1,20 @@
+from collections import Counter
+
+
 def counter_word(text:str):
 	miss=[":",".",";",",","'",'"']
 	for special_elem in miss:
-		text=text.replace(special_elem,"").lower()
-	text=text.split(" ")
-	counted_words={} 
-	for word in text:
-		#know words
-		if word in counted_words:
-			counted_words[word]+=1
-		#Unknow words
-		else:
-			counted_words[word]=1
+		text=text.replace(special_elem,"")
+	text=text.lower().split(" ")
+	counted_words=Counter(text)
+	#counted_words={} 
+	#for word in text:
+	#	#know words
+	#	if word in counted_words:
+	#		counted_words[word]+=1
+	#	#Unknow words
+	#	else:
+	#		counted_words[word]=1
 	return counted_words
 
 text="This is my text text. we're keeping this text short to keep things manageable."
